941bd607e25371a65006dff50ff97eeb43992718
Daniel Machado <daniel.machado.av@gmail.com>
2020-07-27 20:22:14 -0300

    I've changed nothing in the original version

---
+++ Loop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\n   Éoq do programa");
+
+            Console.WriteLine("\n   Loop infinito demonstrativo");
+
+            int value1 = 1;
+
+            int i = 0;
+
+            while (value1 == 1)
+            {
+                i++;
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("\n   0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1");
+                }                
+                else
+                {
+                    Console.WriteLine("\n    0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1");
+                }
+            }
+
+        }
+    }
+}
